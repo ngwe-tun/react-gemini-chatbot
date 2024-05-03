0fceb472d5313a70eab7d28c8c899f2e01809cfd
Ngwe Tun <109126265+ngwe-tun@users.noreply.github.com>
2024-05-03 13:30:12 +0700

    Add App.js and index.js

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import {useState} from "react";
+
+const App = () => {
+    const [value, setValue] = useState("");
+    const [error, setError] = useState("");
+    const [chatHistory, setChatHistory] = useState([]);
+
+    const surpriseOptions = [
+        'Who won the latest NBA championship?',
+        'Where does Human come from?',
+        'How is the political situations in Myanmar?'
+    ]
+
+    const surprise = () => {
+        const randomValue = surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)];
+        setValue(randomValue);
+    }
+
+    const getResponse = async () => {
+        if (!value) {
+            setError("Error! Please ask a question!");
+            return
+        }
+        try {
+            const options = {
+                method: "POST",
+                body: JSON.stringify({
+                    history: chatHistory,
+                    message: value
+                }),
+                headers: {
+                    "content-type": "application/json"
+                }
+            }
+            const response = await fetch('http://localhost:8000/gemini', options)
+            const data = await response.text();
+            console.log(data);
+            setChatHistory(oldChatHistory => [...oldChatHistory, {
+                role: "user",
+                parts: value
+            },
+                {
+                    role: "model",
+                    parts: data
+                }
+            ]);
+            setValue("")
+
+        } catch (error) {
+            console.error(error);
+            setError("Something went wrong!, Please try again later!");
+        }
+    }
+
+    const clear = () => {
+        setValue("")
+        setError("")
+        setChatHistory([])
+    }
+
+    return (
+        <div className="app">
+            <p>
+                What do you want to know?
+                <button className="surprise" onClick={surprise} disabled={!chatHistory}>Surprise me</button>
+            </p>
+            <div className="input-container">
+                <input
+                    value={value}
+                    placeholder="When is Christmas...?"
+                    onChange={(e) => setValue(e.target.value)}
+                />
+                {!error && <button onClick={getResponse}>Ask Me</button>}
+                {error && <button onClick={clear}>Clear</button>}
+            </div>
+            {error && <p>{error}</p>}
+            <div className="search-result">
+                {chatHistory.map((chatItem, _index) => <div key={_index}>
+                    <p className="answer">{chatItem.role} : {chatItem.parts}</p>
+                </div>)}
+            </div>
+        </div>
+    );
+}
+
+export default App;
